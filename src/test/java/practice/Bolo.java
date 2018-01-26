be518a10cb3096429a5fb8105619a48669f483df
rajmuttu76 <35263870+rajmuttu76@users.noreply.github.com>
2018-01-26 20:35:23 +0530

    changed 6
    sop

--- src/test/java/practice/Bolo.java
+++ src/test/java/practice/Bolo.java
@@ -5,7 +5,7 @@ public class Bolo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hiaalssjhss");
-System.out.println("boloodisajhjg");
+System.out.println("boloodisajhjglsdl");
 	}
 
 }
